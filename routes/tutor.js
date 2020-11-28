54d3a3260aaa76d8041eff9a00a4d6d4a88edbb1
Adarsh-S1 <adarshvillasuni@gmail.com>
2020-11-28 14:21:23 +0530

    Tutor panel design completed

--- routes/tutor.js
+++ routes/tutor.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const tutorHelpers = require('../helpers/tutorHelpers');
-const tutorLogin = (req, res,next) => {
+const tutorLogin = (req, res, next) => {
   if (req.session.loggedTutorIn) {
     next()
   } else {
@@ -15,9 +15,10 @@ router.get('/', tutorLogin, (req, res) => {
 });
 router.get('/login', (req, res) => {
   if (req.session.loggedTutorIn) {
-res.redirect('/')  } 
-else {
-    res.render('Tutor/tutorlogin')  
+    res.redirect('/')
+  }
+  else {
+    res.render('Tutor/tutorlogin')
   }
 
 });
@@ -32,24 +33,41 @@ router.post('/login', (req, res) => {
     }
   })
 });
-router.get('/tutorout',function(req,res){
+router.get('/tutorout', function (req, res) {
   req.session.destroy()
   res.redirect('/tutor/login')
 })
-router.get('/students',tutorLogin,function(req,res){
-  res.render('Tutor/studtable',{tutor:true})
+router.get('/students', tutorLogin, function (req, res) {
+  res.render('Tutor/studtable', { tutor: true })
 })
-router.get('/profile',tutorLogin,function(req,res){
-  res.render('Tutor/profile',{tutor:true})
+router.get('/profile', tutorLogin, function (req, res) {
+  res.render('Tutor/profile', { tutor: true })
 })
-router.get('/attendance',tutorLogin,(req,res)=>{
-  res.render('Tutor/Attendance',{tutor:true})
+router.get('/attendance', tutorLogin, (req, res) => {
+  res.render('Tutor/Attendance', { tutor: true })
 })
-router.get('/assignments',tutorLogin,(req,res)=>{
-  res.render('Tutor/assignment',{tutor:true})
+router.get('/assignments', tutorLogin, (req, res) => {
+  res.render('Tutor/assignment', { tutor: true })
 })
-router.get('/notes',tutorLogin,(req,res)=>{
-  res.render('Tutor/notes',{tutor:true})
+router.get('/notes', tutorLogin, (req, res) => {
+  res.render('Tutor/notes', { tutor: true })
 })
-
+router.get('/announcement', tutorLogin, (req, res) => {
+  res.render('Tutor/announcement', { tutor: true })
+})
+router.get('/events', tutorLogin, (req, res) => {
+  res.render('Tutor/Events', { tutor: true })
+})
+router.get('/photos', tutorLogin, (req, res) => {
+  res.render('Tutor/photos', { tutor: true })
+})
+router.get('/addstudent', tutorLogin, (req, res) => {
+  res.render('Tutor/add-student', { tutor: true })
+})
+router.get('/quiz', tutorLogin, (req, res) => {
+  res.render('Tutor/Quiz', { tutor: true })
+})
+router.get('/studetails', tutorLogin, (req, res) => {
+  res.render('Tutor/studetails', { tutor: true })
+})
 module.exports = router;
